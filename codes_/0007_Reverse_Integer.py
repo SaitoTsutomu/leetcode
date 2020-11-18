ea3d876d69c74467467d11d5f739e5c471fba165
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2020-11-19 07:29:48 +0900

    7

--- codes_/0007_Reverse_Integer.py
+++ codes_/0007_Reverse_Integer.py
@@ -4,5 +4,5 @@ import numpy as np
 
 class Solution:
     def reverse(self, x: int) -> int:
-        x = np.sign(x) * int("".join(reversed(str(abs(x)))))
+        x = np.sign(x) * int(str(abs(x))[::-1])
         return x if -(2 ** 31) <= x <= 2 ** 31 - 1 else 0
