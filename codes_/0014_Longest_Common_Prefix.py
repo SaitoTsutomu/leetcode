264e581c4b724277ef21a80513ada0fc337b24ad
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2020-11-22 09:36:08 +0900

    14

--- codes_/0014_Longest_Common_Prefix.py
+++ codes_/0014_Longest_Common_Prefix.py
@@ -1,9 +1,9 @@
 # %% [14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/)
+# 問題：全単語に共通する最長のプレフィックスを返せ
+# 解法：itertools.zip_longestを用いる
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = []
-        for cc in zip(*strs):
+        for i, cc in enumerate(itertools.zip_longest(*strs)):
             if not all(cc[0] == c for c in cc[1:]):
-                break
-            res.append(cc[0])
-        return "".join(res)
+                return strs[0][:i]
+        return strs[0] if strs else ""
